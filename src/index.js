420ff44df044f6561290511fb1974acccb777138
danieljhkim <danieljhkim1@gmail.com>
2022-06-11 14:42:23 -0400

    global data

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ if(false) {
   });
 }
 
+const coinList = ["ethereum,bitcoin,xrp,solana,dogecoin,tether,usd-coin,binancecoin,cardano,binance-usd,polkadot,wrapped-bitcoin,tron,dai,staked-ether,avalanche-2,shiba-inu,leo-token,crypto-com-chain,matic-network,stellar,bitcoin-cash,ftx-token,monero,ethereum-classic,vechai"]
+
 exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     let body = {};
@@ -22,13 +24,19 @@ exports.handler = async (event, context) => {
       'Content-Type': 'application/json',
     };
     try {
-      let resp = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin,xrp,solana,dogecoin,tether&vs_currencies=usd&include_market_cap=true&include_last_updated_at=true&include_24hr_vol=true');
+      let totalMC = 0;
+      let totalVol = 0;
+      let dateTime = "";
+      let resp = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coinList}&vs_currencies=usd&include_market_cap=true&include_last_updated_at=true&include_24hr_vol=true`);
       let respData = resp.data
       body.status = [];
       let i = 0;
       for(let obj in respData) {
         let name = Object.keys(respData)[i++];
         let data = respData[name];
+        totalMC += data.usd_market_cap;
+        totalVol += data.usd_24h_vol;
+        dateTime = data.last_updated_at.toString();
         let db = await putData(name, data.last_updated_at.toString(), data.usd, data.usd_market_cap, data.usd_24h_vol);
         if(db?.$response?.error === null) {
           console.log(`DB update success => ${JSON.stringify(db)}`);
@@ -37,6 +45,7 @@ exports.handler = async (event, context) => {
           body.status.push(`${name} => failed`);
         }
       }
+      const db = await putData("global", dateTime, 0, totalMC, totalVol);
     } catch (err) {
       statusCode = '400';
       body = err.message;
